17b7669784183d156e8ce705ffc518f452dceb37
KirillSozin <sozinov.ka@phystech.edu>
2022-11-26 18:32:10 +0300

    Улучшена скорость работы за счет уменьшения числа проверок. Добавлен 'README.md' и папка с примерами 'examples'

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import numpy as np
 import math
 import random
 
+COUNT_OF_POINTS = 100
 MIN_RADIUS = 0.1
 MIN_Q = 1
 ARENA_SIZE = 20.0
-STEP_ARENA = ARENA_SIZE / 100.0
+STEP_ARENA = ARENA_SIZE / COUNT_OF_POINTS
 
 
 class Point:
@@ -22,6 +23,8 @@ def E(points, x, y):
     Ey = 0.0
     for i in points:
         r = math.sqrt((x - i.x) ** 2 + (y - i.y) ** 2)
+        if r < MIN_RADIUS:
+            return -1
         Ex += i.q * (x - i.x) / r ** 3
         Ey += i.q * (y - i.y) / r ** 3
     E = (Ex ** 2 + Ey ** 2)
@@ -31,18 +34,16 @@ def E(points, x, y):
 def go_E(points):
     ans = []
     for i in np.arange(-ARENA_SIZE, ARENA_SIZE, STEP_ARENA):
+        print("progress: " + str(int((i + ARENA_SIZE) / (2 * ARENA_SIZE) * 100)) + "%", end="\r")
         for j in np.arange(-ARENA_SIZE, ARENA_SIZE, STEP_ARENA):
-            ok = True
-            for p in points:
-                r = math.sqrt((i - p.x) ** 2 + (j - p.y) ** 2)
-                if r < MIN_RADIUS:
-                    ok = False
-            if ok:
-                ans.append([i, j, E(points, i, j)])
+            tmpE = E(points, i, j)
+            tmp = [i, j, tmpE]
+            if tmp[2] > 0:
+                ans.append(tmp)
     return ans
 
 
-def plot_points(points):
+def draw_field(points):
     print("go_E starting...")
     p = go_E(points)
 
@@ -68,19 +69,19 @@ def plot_points(points):
             plt.scatter(i.x, i.y, color='green', s=15)
 
     plt.ion()
-    while True:
-        plt.show()
-        plt.pause(0.1)
-        # fast get and print mouse coordinates
-        print("E = ", E(points, plt.ginput(1, timeout=0)[0][0], plt.ginput(1, timeout=0)[0][1]), "V/m")
+    plt.show()
+    plt.pause(0.001)
+    input("Press Enter to continue...")
+    plt.close()
+
 
 
 def generate_points(n=10):
     # random points
     given_points = []
     for i in range(n):
-        given_points.append(Point(random.randrange(-ARENA_SIZE, ARENA_SIZE), random.randrange(-ARENA_SIZE, ARENA_SIZE),
-                                  ((-1) ** random.random(2)) * MIN_Q))
+        given_points.append(
+            Point(random.uniform(-ARENA_SIZE, ARENA_SIZE), random.uniform(-ARENA_SIZE, ARENA_SIZE), MIN_Q))
     return given_points
 
 
@@ -93,10 +94,10 @@ def generate_line(a=1, b=0, start=-ARENA_SIZE, end=ARENA_SIZE):
     return given_points
 
 
-def generate_circle(r=10, start=-ARENA_SIZE, end=ARENA_SIZE):
+def generate_circle(r=10, n=COUNT_OF_POINTS):
     # circle
     given_points = []
-    for i in np.arange(start, end, STEP_ARENA):
+    for i in np.arange(0, 2 * math.pi, 2 * math.pi / n):
         given_points.append(Point(r * math.cos(i), r * math.sin(i), MIN_Q))
     return given_points
 
@@ -111,9 +112,9 @@ def generate_parabola(a=1, b=0, c=0, start=-ARENA_SIZE, end=ARENA_SIZE):
 
 
 def main():
-    # example - two circles
-    given_points = generate_circle(10) + generate_circle(15)
-    plot_points(given_points)
+    # example - 10 random points
+    given_points = generate_points()
+    draw_field(given_points)
 
 
 if __name__ == '__main__':
